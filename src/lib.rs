f32baf713cc6bb0b7672ba2e4ab051cbd2db49fd
Caleb Hattingh <caleb.hattingh@gmail.com>
2024-07-07 18:17:47 +0200

    Use closures to only create the bufreaders as needed.

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,15 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 pub struct FileInput {
-    readers: Vec<Box<dyn BufRead>>,
+    // Using closures to lazily open files
+    readers: Vec<Box<dyn Fn() -> io::Result<Box<dyn BufRead>>>>,
     current_reader: usize,
+    current_r: Box<dyn BufRead>
 }
 
 impl FileInput {
     pub fn new() -> io::Result<Self> {
-        let mut readers: Vec<Box<dyn BufRead>> = Vec::new();
+        let mut readers: Vec<Box<dyn Fn() -> io::Result<Box<dyn BufRead>>>> = Vec::new();
         let args: Vec<String> = env::args().collect();
         let mut reading_files = false;
 
@@ -24,19 +26,24 @@ impl FileInput {
             }
 
             if reading_files {
-                let file = File::open(arg)?;
-                readers.push(Box::new(BufReader::new(file)));
+                let arg = arg.to_string();
+                readers.push(Box::new(move || {
+                    let file = File::open(&arg)?;
+                    Ok(Box::new(BufReader::new(file)) as Box<dyn BufRead>)
+                }));
             }
         }
 
         if readers.is_empty() {
-            // If no files were specified, read from stdin
-            readers.push(Box::new(BufReader::new(io::stdin())));
+            readers.push(Box::new(|| Ok(Box::new(BufReader::new(io::stdin())) as Box<dyn BufRead>)));
         }
+
+        let first_reader = readers[0]()?;
 
         Ok(FileInput {
             readers,
             current_reader: 0,
+            current_r: first_reader,
         })
     }
 }
@@ -45,21 +52,23 @@ impl Iterator for FileInput {
     type Item = io::Result<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if self.current_reader >= self.readers.len() {
-                return None;
-            }
+        if self.current_reader >= self.readers.len() {
+            return None;
+        }
 
-            let mut line = String::new();
-            match self.readers[self.current_reader].read_line(&mut line) {
-                Ok(0) => {
-                    // End of current file, move to next reader
-                    self.current_reader += 1;
-                    continue;
+        let mut line = String::new();
+        match self.current_r.read_line(&mut line) {
+            Ok(0) => {
+                self.current_reader += 1;
+                if self.current_reader >= self.readers.len() {
+                    None
+                } else {
+                    self.current_r = self.readers[self.current_reader]().unwrap();
+                    self.next()
                 }
-                Ok(_) => return Some(Ok(line.trim_end().to_string())),
-                Err(e) => return Some(Err(e)),
-            }
+            },
+            Ok(_) => return Some(Ok(line.trim_end().to_string())),
+            Err(e) => Some(Err(e)),
         }
     }
 }
